c84be9710c5acad39e31a19a343a11874d2dc81c
John Armstrong <jlarmst@g.clemson.edu>
2017-08-16 14:50:57 -0400

    incorrect array response

--- main.js
+++ main.js
@@ -37,15 +37,15 @@ if (program.users) {
 }
 console.log(chalk.blue(arrUsers));
 
-// let token = 'cd998afc37e5104e2e1323343bd4cfd212e2887d';
-// let requestifyOptions = requestifyOptionsFunc(token);
-let requestifyOptions = requestifyOptionsFunc();
+let token = 'cd998afc37e5104e2e1323343bd4cfd212e2887d';
+let requestifyOptions = requestifyOptionsFunc(token);
+// let requestifyOptions = requestifyOptionsFunc();
 
 let masterRepoUrls = [];
 
 function requestifyUrlFunc (user, page) {
   let url = 'https://api.github.com/users/' + user + '/repos?page=' + page;
-  console.log(chalk.blue(url));
+  // console.log(chalk.blue(url));
   return url;
 }
 
@@ -60,111 +60,103 @@ function requestifyOptionsFunc (token) {
   }
   return requestifyOptions;
 }
-// function requestifyOptionsFunc (token) {
-//   let requestifyOptions = {};
-//   if (token) {
-//     console.log(chalk.blue(token));
-//     token = 'token ' + token;
-//     requestifyOptions.headers = {
-//       Authorization: token
-//     }
-//   }
-//   return requestifyOptions;
-// }
 
-// ============= it freaking works
 function getRequestify (requestifyUrl, requestifyOptions) {
-  return fetch(requestifyUrl, requestifyOptions)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      let newRepoUrls = [];
-      for (var i = 0; i < json.length; i++) {
-        let htmlUrl = json[i].html_url;
-        console.log(htmlUrl);
-        newRepoUrls.push(htmlUrl);
-      }
-      return newRepoUrls;
-    }).catch(function(err) {
-      console.log(err);
-    })
+  console.log('start getRequestify');
+  return requestify.get(requestifyUrl, requestifyOptions)
+  .then(function(response) {
+    console.log('then getRequestify');
+    let body = response.getBody();
+    let newRepoUrls = [];
+    for (var i = 0; i < body.length; i++) {
+      let htmlUrl = body[i].html_url;
+      console.log(htmlUrl);
+      newRepoUrls.push(htmlUrl);
+    }
+    return newRepoUrls;
+  }).catch(function(err) {
+    console.log('fail getRequestify');
+    console.log(err);
+  });
 }
 
-// =================== it also works
-// function getRequestify (requestifyUrl, requestifyOptions) {
-//   console.log('getRequestify');
-//   requestify.get(requestifyUrl, requestifyOptions)
-//   .then(function(response) {
-//     console.log('========breakline========');
-//     let body = response.getBody();
-//     let newRepoUrls = [];
-//     for (var i = 0; i < body.length; i++) {
-//       let htmlUrl = body[i].html_url;
-//       console.log(htmlUrl);
-//       newRepoUrls.push(htmlUrl);
-//     }
-//     return newRepoUrls;
-//   }).catch(function (err) {
-//     console.log('fail getRequestify');
-//     return null;
-//   });
-// }
-
-
-
-
-// function getRepoUrls (repoUrls, requestifyUrl, requestifyOptions) {
-//   var myPromise = new Promise(function(resolve, reject) {
-//     getRequestify(requestifyUrl, requestifyOptions).then(function(response){
-//       console.log(chalk.magenta('start getRepoUrls'));
-//       repoUrls = repoUrls.push(response);
-//       console.log('yolo');
-//       resolve(repoUrls);
-//     });
-//   }
-//   return myPromise;
-// }
-// function getRepoUrls (repoUrls, requestifyUrl, requestifyOptions) {
-//   getRequestify(requestifyUrl, requestifyOptions).then(function(response){
-//     console.log(chalk.magenta('start getRepoUrls'));
-//     repoUrls = repoUrls.push(response);
-//     console.log('yolo');
-//     return repoUrls;
-//   }).catch(function(err){
-//     console.log('fail getRepoUrls');
-//     return null;
-//   });
-// }
 function getRepoUrls (repoUrls, requestifyUrl, requestifyOptions) {
-  console.log(chalk.magenta('start getRepoUrls'));
-  repoUrls = repoUrls.push(getRequestify(requestifyUrl, requestifyOptions))
-  return repoUrls;
+  console.log('start repoUrls');
+  return getRequestify(requestifyUrl, requestifyOptions).then(function(response){
+    repoUrls.push(response);
+    console.log('then getRepoUrls');
+    return repoUrls;
+  }).catch(function(err) {
+    console.log('fail getRepoUrls');
+    console.log(err);
+  });
 }
+// function getRepoUrls (repoUrls, requestifyUrl, requestifyOptions) {
+//   console.log(chalk.magenta('start getRepoUrls'));
+//   repoUrls = repoUrls.push(getRequestify(requestifyUrl, requestifyOptions))
+//   return repoUrls;
+// }
 
 function concatRepoUrls (repoUrls, user, page, requestifyOptions) {
+  console.log('start concatRepoUrls');
   repoLen = repoUrls.length;
   let requestifyUrl = requestifyUrlFunc(user, page);
-  let addRepoUrls = getRepoUrls(repoUrls, requestifyUrl, requestifyOptions);
-  if (addRepoUrls.length) {
-    repoUrls = repoUrls.concat(addRepoUrls);
-  }
-  if (repoUrls.length > repoLen) {
-    console.log(chalk.blue('go again'));
-    page++;
-    requestifyUrl = requestifyUrlFunc(user, page)
-    getRepoUrls(repoUrls, requestifyUrl, requestifyOptions);
-  }
-  console.log(repoUrls);
-  return repoUrls;
+  return getRepoUrls(repoUrls, requestifyUrl, requestifyOptions).then(function(response) {
+    console.log('then concatRepoUrls');
+    let addRepoUrls = response;
+    console.log(addRepoUrls.length);
+    if (addRepoUrls.length) {
+      console.log('recursive getRepoUrls');
+      repoUrls.concat(addRepoUrls);
+      page++;
+      console.log('user: ' + user + ', page: ' + page);
+      requestifyUrl = requestifyUrlFunc(user, page);
+      return getRepoUrls(repoUrls, requestifyUrl, requestifyOptions);
+    }
+    return repoUrls;
+  }).catch(function(err) {
+    console.log('fail concatRepoUrls');
+    console.log(err);
+  })
 }
+// function concatRepoUrls (repoUrls, user, page, requestifyOptions) {
+//   repoLen = repoUrls.length;
+//   let requestifyUrl = requestifyUrlFunc(user, page);
+//   let addRepoUrls = getRepoUrls(repoUrls, requestifyUrl, requestifyOptions);
+//   if (addRepoUrls.length) {
+//     repoUrls = repoUrls.concat(addRepoUrls);
+//   }
+//   if (repoUrls.length > repoLen) {
+//     console.log(chalk.blue('go again'));
+//     page++;
+//     requestifyUrl = requestifyUrlFunc(user, page)
+//     getRepoUrls(repoUrls, requestifyUrl, requestifyOptions);
+//   }
+//   console.log(repoUrls);
+//   return repoUrls;
+// }
 
 function userRepoUrls (user, requestifyOptions) {
+  console.log('start userRepoUrls');
   let page = 1;
   let repoUrls = [];
-  let oldLen = repoUrls.length;
-  let userRepoUrls = concatRepoUrls(repoUrls, user, page, requestifyOptions, oldLen)
-  return userRepoUrls;
+  return concatRepoUrls(repoUrls, user, page, requestifyOptions).then(function(response) {
+    console.log('then userRepoUrls');
+    let userRepoUrls = response;
+    console.log(response); //broken here
+    return userRepoUrls;
+  }).catch(function(err) {
+    console.log('fail userRepoUrls');
+    console.log(err);
+  });
 }
+// function userRepoUrls (user, requestifyOptions) {
+//   let page = 1;
+//   let repoUrls = [];
+//   let oldLen = repoUrls.length;
+//   let userRepoUrls = concatRepoUrls(repoUrls, user, page, requestifyOptions, oldLen)
+//   return userRepoUrls;
+// }
 
 for (var i = 0; i < arrUsers.length; i++) {
   let user = arrUsers[i];
